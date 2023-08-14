570f148bb35bbff768058b6ddd2a0e8fef332aab
KimYoungUk <dmnion@naver.com>
2023-08-14 15:33:54 +0900

    fds

---
+++ src/bj/g5/BJ_G5_10026_적록색약.java
@@ -0,0 +1,36 @@
+package bj.g5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+@author 김영욱
+@since 2023. 08. 14
+@see https://www.acmicpc.net/problem/10026
+@git
+@performance
+@category #
+@note
+시간 : 1초(1억번) N:100 N^2이여도 시간 통과 가능
+RGB가 들어있는 2차원 배열이 있다.
+근데 색약있는 사람은 RG를 같은 색으로 본다.
+두 사람이 인식하는 구역의 개수를 구해라.
+
+BFS를 먼저 생각했다.
+visited 배열도 만든다(N*N)
+N*N을 탐색하면서
+*/
+public class BJ_G5_10026_적록색약 {
+
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder builder = new StringBuilder();
+	static StringTokenizer tokens;
+	
+	static char[][] map;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
