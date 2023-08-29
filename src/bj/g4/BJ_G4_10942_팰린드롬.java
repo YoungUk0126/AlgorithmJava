6af1a0a34f360b9979b921784c08ac90cd6f70de
김영욱 <dmnion@naver.com>
2023-08-29 17:51:37 +0900

    ㄹㅇㄴ

--- src/bj/g4/BJ_G4_10942_팰린드롬.java
+++ src/bj/g4/BJ_G4_10942_팰린드롬.java
@@ -1,5 +1,18 @@
 package bj.g4;
+/**
+@author 김영욱
+@since 2023. 08. 29
+@see https://www.acmicpc.net/problem/10942
+@git
+@performance
+@category #
+@note
+팰린드롬은 역삼역 같이 거꾸로 해도 같은걸 말함
+2차원 배열을 만들어서 미리 싹다 1 5라면 1~n까지 팰린드롬인지 아닌지 계산해서 넣어놓고
+질문이 들어오면 바로바로 인덱스에서 조회해서 출력할 수 있도록 만들까 구상중이다.
 
+미래의 나 화이팅!
+*/
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
