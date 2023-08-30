a383a04680be51976cf1ad4b0a23ef55d8df4754
DESKTOP-5P94ADK\김영욱 <dmnion@naver.com>
2023-08-30 22:30:31 +0900

    fds

--- src/bj/g4/BJ_G4_17404_RGB거리2.java
+++ src/bj/g4/BJ_G4_17404_RGB거리2.java
@@ -11,17 +11,14 @@ public class BJ_G4_17404_RGB거리2 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer tokens;
 	
-	static int n, arr[][], dp[][];
+	static int n, arr[][], dp[][],paints[];
 	
 	public static void main(String[] args) throws IOException{
 		n = Integer.parseInt(input.readLine());
 		
 		arr = new int[n][3];
 		dp = new int[n][3];
-		
-		for(int i=0; i<n; i++) {
-			Arrays.fill(dp[i], Integer.MAX_VALUE - 1001);
-		}
+		paints = new int[3];
 		
 		for(int i=0; i<n; i++) {
 			tokens = new StringTokenizer(input.readLine());
@@ -32,9 +29,11 @@ public class BJ_G4_17404_RGB거리2 {
 		
 		// 빨간색 집, 파란색 집, 초록색 집 가는거 따로 계산해줘야함(고쳐라 여기)
 		for(int l=0; l<3; l++) {
+			for(int i=0; i<n; i++) {
+				Arrays.fill(dp[i], Integer.MAX_VALUE - 1001);
+			}
 			for(int i=0; i<3; i++) {
 				if(i == l)dp[0][i] = arr[0][i];
-				else dp[0][i] = Integer.MAX_VALUE - 1001;
 			}
 			for(int i=1; i<n; i++) {
 				for(int j=0; j<3; j++) {
@@ -45,17 +44,18 @@ public class BJ_G4_17404_RGB거리2 {
 					}
 				}
 			}
-			for(int i=0; i<n; i++) {
-				for(int j=0; j<3; j++) {
-					System.out.print(dp[i][j] + " ");
-				}
-				System.out.println();
+			if(l == 0) {
+				paints[l] = Math.min(dp[n-1][1], dp[n-1][2]);
 			}
-			System.out.println();
-			System.out.println();
+			else if(l == 1) {
+				paints[l] = Math.min(dp[n-1][0], dp[n-1][2]);
+			}
+			else {
+				paints[l] = Math.min(dp[n-1][0], dp[n-1][1]);
+			}
 		}
-		int a = Math.min(dp[n-1][0], dp[n-1][1]);
-		int b = Math.min(dp[n-1][1], dp[n-1][2]);
+		int a = Math.min(paints[0], paints[1]);
+		int b = Math.min(paints[1], paints[2]);
 		System.out.println(Math.min(a, b));
 		
 	}
