d230cc29a457f06757efd656d4b1924c21aeff3f
DESKTOP-5P94ADK\김영욱 <dmnion@naver.com>
2023-09-06 22:47:28 +0900

    fds

--- src/bj/g3/BJ_G3_1005_ACMCraft.java
+++ src/bj/g3/BJ_G3_1005_ACMCraft.java
@@ -1,3 +1,4 @@
+package bj.g3;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/bj/g4/BJ_G4_20040_사이클게임.java
+++ src/bj/g4/BJ_G4_20040_사이클게임.java
@@ -1,3 +1,5 @@
+package bj.g4;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
