cafdb5f738508ff2b3c39964664ae3e29baf9644
김영욱 <dmnion@naver.com>
2023-09-13 12:06:27 +0900

    ㄹㅇㄴ

--- src/bj/g3/BJ_G3_2473_세용액.java
+++ src/bj/g3/BJ_G3_2473_세용액.java
@@ -20,7 +20,9 @@ import java.util.StringTokenizer;
 
 두 용액처럼 정렬 후 투포인터를 쓰는 것은 확실한데
 용액을 한 가지 더 섞어야 하는 문제다.
-양 옆에 
+0,1부터 두개의 포인터를 출발시키자
+두 개의 포인터를 찍은 후 나머지 인덱스를 이분탐색하여 적절한 숫자를 찾아서 더한다
+그 수가 min보다 0에 가깝다면 min을 업데이트 한다.
 */
 public class BJ_G3_2473_세용액 {
 	
@@ -28,7 +30,7 @@ public class BJ_G3_2473_세용액 {
 	static StringBuilder builder = new StringBuilder();
 	static StringTokenizer tokens;
 	
-	static int N, left, right, arr[], min;
+	static int N, left, right, arr[], min, mid;
 	
 
 	public static void main(String[] args) throws IOException{
@@ -47,6 +49,10 @@ public class BJ_G3_2473_세용액 {
 		int sum = 0;
 		while( left <= right ) {
 			sum = arr[left] + arr[right];
+			mid = BinarySearch(right+1, sum);
+			if (sum + arr[mid] == 0) {
+				break;
+			}
 			if(min > Math.abs(sum)) {
 				
 			}
@@ -55,6 +61,34 @@ public class BJ_G3_2473_세용액 {
 			}
 			
 		}
+		int ans[] = new int[3];
+		ans[0] = arr[left];
+		ans[1] = arr[right];
+		ans[2] = arr[mid];
+		Arrays.sort(ans);
+		for(int i=0; i<3; i++) {
+			System.out.print(ans[i] + " ");
+		}
+	}
+
+
+	private static int BinarySearch(int left, int sum) {
+		int right = N-1;
+		int mid = 0;
+		
+		while(left<=right) {
+			mid = (left + right) / 2;
+			if(sum + arr[mid] == 0) {
+				return mid;
+			}
+			else if(sum+arr[mid] < 0) {
+				left = mid+1;
+			}
+			else if(sum+arr[mid] > 0) {
+				right = mid-1;
+			}
+		}
+		return mid;
 	}
 
 }
