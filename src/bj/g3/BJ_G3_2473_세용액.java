231c0606e251d991a089496badda33080798ec81
김영욱 <dmnion@naver.com>
2023-09-13 17:58:09 +0900

    ㄹㅇㄴ

--- src/bj/g3/BJ_G3_2473_세용액.java
+++ src/bj/g3/BJ_G3_2473_세용액.java
@@ -20,8 +20,9 @@ import java.util.StringTokenizer;
 
 두 용액처럼 정렬 후 투포인터를 쓰는 것은 확실한데
 용액을 한 가지 더 섞어야 하는 문제다.
-0,1부터 두개의 포인터를 출발시키자
-두 개의 포인터를 찍은 후 나머지 인덱스를 이분탐색하여 적절한 숫자를 찾아서 더한다
+for문을 N-2까지 돌리고 그 용액은 고정이다.
+선택된 용액 +1 ,끝부터 두개의 포인터를 출발시키자
+두 개의 포인터를 찍은 후 값을 비교하여 left, right를 이동시킨다.
 그 수가 min보다 0에 가깝다면 min을 업데이트 한다.
 */
 public class BJ_G3_2473_세용액 {
@@ -30,14 +31,13 @@ public class BJ_G3_2473_세용액 {
 	static StringBuilder builder = new StringBuilder();
 	static StringTokenizer tokens;
 	
-	static int N, left, right, arr[], min, mid, ans[], ansLeft;
+	static int N, left, right, arr[], mid, ans[], ansLeft;
+	static long min;
 	
 
 	public static void main(String[] args) throws IOException{
 		N = Integer.parseInt(input.readLine());
 		arr = new int[N];
-		left = 0;
-		right = 1;
 		min = Integer.MAX_VALUE;
 		ans = new int[3];
 		
@@ -47,51 +47,31 @@ public class BJ_G3_2473_세용액 {
 		}
 		
 		Arrays.sort(arr);
-		int sum = 0;
-		while( left <= right ) {
-			sum = arr[left] + arr[right];
-			mid = BinarySearch(right+1, sum);
-			if (sum + arr[mid] == 0) {
-				break;
+		long sum = 0;
+		for(int i=0; i<N-2; i++) {
+			left = i+1;
+			right = N-1;
+			while( left < right ) {
+				sum = (long)arr[left] + arr[right] + arr[i];
+				if(min > Math.abs(sum)) {
+					min = Math.abs(sum);
+					ans[0] = arr[left];
+					ans[1] = arr[right];
+					ans[2] = arr[i];
+				}
+				else if(sum > 0) {
+					right--;
+				}
+				else {
+					left++;
+				}
 			}
-			if(sum > 0) {
-				right--;
-			}
-			else {
-				left++;
-			}
-			
 		}
-		ans[0] = arr[left];
-		ans[1] = arr[right];
-		ans[2] = arr[mid];
+		
 		Arrays.sort(ans);
 		for(int i=0; i<3; i++) {
 			System.out.print(ans[i] + " ");
 		}
-	}
-
-
-	private static int BinarySearch(int left, int sum) {
-		int right = N-1;
-		int mid = 0;
-		
-		while(left<=right) {
-			mid = (left + right) / 2;
-			if(sum + arr[mid] == 0) {
-				return mid;
-			}
-			if(min > Math.abs(sum + arr[mid])) {
-				min = Math.abs(sum + arr[mid]);
-			}
-			else if(sum+arr[mid] < 0) {
-				left = mid+1;
-			}
-			else if(sum+arr[mid] > 0) {
-				right = mid-1;
-			}
-		}
-		return mid;
 	}
 
 }
