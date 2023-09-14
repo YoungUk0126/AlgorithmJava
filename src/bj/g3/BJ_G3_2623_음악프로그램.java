543b3460603312818da9053b942a4c02cc327562
김영욱 <dmnion@naver.com>
2023-09-14 13:50:21 +0900

    fds

---
+++ src/bj/g3/BJ_G3_2623_음악프로그램.java
@@ -0,0 +1,35 @@
+package bj.g3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+@author 김영욱
+@since 2023. 09.14
+@see https://www.acmicpc.net/problem/2623
+@git
+@performance
+@category #
+@note
+음악프로그램 가수 출연 순서를 정해야한다.
+근데 보조PD의 수 M명이 출연 순서를 제출한다.
+ 1 4 3
+ 6 2 5 4
+ 2 3
+ 이 순서를 지켜서 가수 N명(여기선 6명)의 출연 순서를 정해라.
+*/
+public class BJ_G3_2623_음악프로그램 {
+	
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder builder = new StringBuilder();
+	static StringTokenizer tokens;
+	
+	static int N,M;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
