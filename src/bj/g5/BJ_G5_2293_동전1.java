4366be1ef71394d6b7ee8221e7359ebdbaf30f2f
DESKTOP-5P94ADK\김영욱 <dmnion@naver.com>
2023-10-15 23:15:16 +0900

    ㄹㄴ

--- src/bj/g5/BJ_G5_2293_동전1.java
+++ src/bj/g5/BJ_G5_2293_동전1.java
@@ -2,12 +2,13 @@ package bj.g5;
 /**
 @author 김영욱
 @since 2023. 10. 15.
-@see 
+@see https://www.acmicpc.net/problem/2293
 @git
-@performance 
+@performance 84ms
 @category #
 @note
 
+*************내가 안풀었음****************
 n가지 종류의 동전이 있다. 각각의 동전이 나타내는 가치는 다르다.
 이 동전을 적당히 사용해서, 그 가치의 합이 k원이 되도록 하고 싶다.
 그 경우의 수를 구하시오. 각각의 동전은 몇 개라도 사용할 수 있다.
@@ -21,7 +22,7 @@ n가지 종류의 동전이 있다. 각각의 동전이 나타내는 가치는 �
 dp[j] += dp[j - coins[i]]
 i가지 동전을 사용했을 때 가치의 합이 j원이 되는 가짓수 += j원 - 현재 동전 가치
 
-
+dp문제는 앞으로 표를 먼저 그리고 그 다음 점화식을 찾아내는게 더 편할 것 같다.
 
 **/
 import java.io.BufferedReader;
@@ -48,9 +49,7 @@ public class BJ_G5_2293_동전1 {
 		// money만큼의 가치가 되려고 함.
 		money = Integer.parseInt(tokens.nextToken());
 		coins = new int[N+1];
-		
 		// 코인의 종류들을 구함
-		
 		dp = new int[money+1];
 		dp[0] = 1;
 		
