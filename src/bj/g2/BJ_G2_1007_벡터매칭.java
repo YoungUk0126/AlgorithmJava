d03ad348d418590f7f36a392b3831ef979e6a807
김영욱 <dmnion@naver.com>
2023-10-19 17:59:38 +0900

    fs

---
+++ src/bj/g2/BJ_G2_1007_벡터매칭.java
@@ -0,0 +1,31 @@
+package bj.g2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_G2_1007_벡터매칭 {
+	
+	
+	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	StringBuilder builder = new StringBuilder();
+	StringTokenizer tokens;
+	
+	static int N,T;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	static class cord {
+		int x, y;
+		
+		public cord(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+	}
+
+}
