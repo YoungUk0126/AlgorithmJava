f2b64df04ed9a2b88495083a16b7fa17e4783c44
KimYoungUk <dmnion@naver.com>
2025-01-10 03:01:24 +0900

    하... 정수로 바꿔서 하면 이렇게 쉽구나...

--- src/bj/g3/BJ_G3_2457_공주님의정원.java
+++ src/bj/g3/BJ_G3_2457_공주님의정원.java
@@ -3,10 +3,7 @@ package bj.g3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BJ_G3_2457_공주님의정원 {
 
@@ -19,6 +16,7 @@ public class BJ_G3_2457_공주님의정원 {
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(input.readLine());
         flowers = new ArrayList<>();
+        Stack<Flower> answerList = new Stack<>();
         for(int i=0; i<N; i++) {
             tokens = new StringTokenizer(input.readLine());
             int startMonth = Integer.parseInt(tokens.nextToken());
@@ -33,12 +31,55 @@ public class BJ_G3_2457_공주님의정원 {
         }
         Collections.sort(flowers);
 
+        int start = 301;
+        int end = 1201;
+        int nextSearchIndex = 0;
+        int choosen = 0;
+        int choosenStart = 0;
+        int answer = 0;
+        while(start < end) {
+            boolean isFind = false;
+
+            for(int i=nextSearchIndex; i<flowers.size(); i++) {
+                Flower now = flowers.get(i);
+                if(now.start > start) break;
+
+                if(choosen < now.end) {
+                    choosen = now.end;
+                    choosenStart = now.start;
+                    isFind = true;
+                }
+            }
+            if(isFind) {
+                start = choosen;
+                answer++;
+                answerList.push(new Flower(choosenStart, choosen));
+            }
+            else {
+                break;
+            }
+        }
+        if(start >= end) {
+            for (Flower now: answerList) System.out.println(now);
+            System.out.println(answer);
+        } else {
+            System.out.println(0);
+        }
+
 
     }
 
     private static class Flower implements Comparable<Flower> {
         int start;
         int end;
+
+        @Override
+        public String toString() {
+            return "Flower{" +
+                           "start=" + start +
+                           ", end=" + end +
+                           '}';
+        }
 
         public Flower(int start, int end) {
             this.start = start;
