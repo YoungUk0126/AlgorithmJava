e4d7fcde613aabb9dca04529a4b83ac985b7a84a
KimYoungUk <dmnion@naver.com>
2025-02-06 01:50:34 +0900

    어렵다 어려워

--- src/bj/g4/BJ_G4_2448_별찍기11.java
+++ src/bj/g4/BJ_G4_2448_별찍기11.java
@@ -1,58 +1,64 @@
 package bj.g4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 /**
- @author 김영욱
- @since 2025. 01. 29.
- @see https://www.acmicpc.net/problem/2448
- @git
- @performance
- @category #
- @note
- 첫째 줄에 N이 주어진다. N은 항상 3×2k 수이다. (3, 6, 12, 24, 48, ...) (0 ≤ k ≤ 10, k는 정수)
- N = 23
-             *
-            * *
-           *****
-          *     *
-         * *   * *
-        ***** *****
-       *           *
-      * *         * *
-     *****       *****
-    *     *     *     *
-   * *   * *   * *   * *
-  ***** ***** ***** *****
-
- 12번째 줄 5 1 패턴 4번( 12/3 )
- 그 윗줄 1 1 1 3 4번
- 그 윗줄 1 5 1 5 1 5 1 5 4번
-
-
- 첫번째 공백은 N-1부터 1씩줄음
-
-
-  *     *
-**/
+ * @author 김영욱
+ * @git
+ * @performance
+ * @category #
+ * @note 첫째 줄에 N이 주어진다. N은 항상 3×2k 수이다. (3, 6, 12, 24, 48, ...) (0 ≤ k ≤ 10, k는 정수)
+ * N = 12
+ * N을 2씩 나누면서 3이 되었을 때 가운데가 비어있는 큰 삼각형을 출력
+ * 바보라서 인터넷 보고 이해했음
+ *
+ *
+ * @see https://www.acmicpc.net/problem/2448
+ * @since 2025. 01. 29.
+ **/
 
 public class BJ_G4_2448_별찍기11 {
 
-    public static void main(String[] args) {
-        for (int i=0; i<23; i++) {
-            System.out.print(" ");
+    static StringBuilder builder = new StringBuilder();
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+    static char[][] stars;
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(input.readLine());
+
+        stars = new char[N][2 * N - 1];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(stars[i], ' ');
         }
-        System.out.println("*");
-        for (int i=0; i<22; i++) {
-            System.out.print(" ");
+
+        star(0, N - 1, N);
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<2*N-1; j++) {
+                builder.append(stars[i][j]);
+            }
+            builder.append("\n");
         }
-        System.out.print("*");
-        System.out.print(" ");
-        System.out.println("*");
-        for (int i=0; i<21; i++) {
-            System.out.print(" ");
-        }
-        for(int i=0; i<5; i++) {
-            System.out.print("*");
-        }
-        System.out.println();
+        System.out.println(builder);
     }
+
+    private static void star(int r, int c, int N) {
+        if (N == 3) {
+            stars[r][c] = '*';//*
+            stars[r + 1][c - 1] = stars[r + 1][c + 1] = '*';//* *
+            stars[r + 2][c - 2] = stars[r + 2][c - 1] = stars[r + 2][c] = stars[r + 2][c + 1] = stars[r + 2][c + 2] = '*';//*****
+        } else {
+            int cut = N / 2;
+            star(r, c, cut);// 제일 위에 삼각형
+            star(r + cut, c - cut, cut);// 아래 왼쪽 삼각형
+            star(r + cut, c + cut, cut);// 아래 오른쪽 삼각형
+        }
+    }
+
 }
