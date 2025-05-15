df1c4cd090e680ed37ae2cd17970c746ddfb204b
KimYoungUk <dmnion@naver.com>
2025-05-16 02:28:39 +0900

    어려워 뒤지겠네 진짜

--- src/bj/g2/BJ_G2_12015_가장긴증가하는부분수열2.java
+++ src/bj/g2/BJ_G2_12015_가장긴증가하는부분수열2.java
@@ -1,12 +1,18 @@
 package bj.g2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections; // 순서 뒤집기 위해 추가
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+/*
+반례
+25
+10 12 14 11 15 22 1 21 8 24 8 20 3 2 5 6 17 7 16 25 19 23 18 13 3
+
+이분 탐색 결과 : 1 2 3 7 13 18 25
+
+본래 답 : 1 2 5 6 7 16 19 23
+*
+* */
 
 
 public class BJ_G2_12015_가장긴증가하는부분수열2 {
@@ -20,12 +26,38 @@ public class BJ_G2_12015_가장긴증가하는부분수열2 {
     public static void main(String[] args) throws IOException {
 
         N = Integer.parseInt(input.readLine());
-        tokens = new StringTokenizer(input.readLine());
         lines = new int[N];
-        for(int i=0; i<N; i++) {
+        dp = new ArrayList<>();
+
+        tokens = new StringTokenizer(input.readLine());
+        for (int i = 0; i < N; i++) {
             lines[i] = Integer.parseInt(tokens.nextToken());
         }
+        for (int line : lines) {
+            BinarySearch(line);
+        }
+        System.out.println(dp.size());
 
 
+    }
+
+    private static void BinarySearch(int line) {
+        if (dp.isEmpty()) dp.add(line);
+        if (dp.get(0) > line) dp.set(0, line);
+        if (dp.get(dp.size() - 1) < line) dp.add(line);
+        else {
+            int left = 0;
+            int right = dp.size() - 1;
+            int mid = 0;
+            while (left < right) {
+                mid = (left + right) / 2;
+                if (dp.get(mid) < line) {// 중간 값보다 크다면 왼쪽 영역을 위로 댕겨와야해
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            dp.set(left, line);
+        }
     }
 }
